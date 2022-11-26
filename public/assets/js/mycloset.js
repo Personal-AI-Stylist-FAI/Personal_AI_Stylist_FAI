8c958bcd7ff280b49c4a5c0daefa7c84fd94d56b
onda0_0 <qwen0524@gmail.com>
2022-11-26 21:41:35 +0900

    마이클로젯에서 ai가 분류한 세부항목 출력되도록 수정 완료

--- public/assets/js/mycloset.js
+++ public/assets/js/mycloset.js
@@ -71,9 +71,10 @@ $(document).ready(function() {
     $("#add").click(getCloset);
     
     getCloset();
+    // getClosetClass();
 });
 
-function getCloset() { // 옷 추가
+function getCloset() { // 사용자 의류 이미지 화면에 출력
     $.ajax({
         type: "GET",
         url: "http://localhost:3000/mycloset/myclosetclothes",
@@ -85,7 +86,7 @@ function getCloset() { // 옷 추가
             let outer_address_array = [];
 
             var image_address = "";
-
+/* 
             for(var i=0; i<response['top'].length; i++) {
                 image_address = RELATIVE_PATH + response['top'][i];
                 top_address_array.push(image_address);
@@ -104,26 +105,80 @@ function getCloset() { // 옷 추가
             for(var i=0; i<response['outer'].length; i++) {
                 image_address = RELATIVE_PATH + response['outer'][i];
                 outer_address_array.push(image_address);
+            } 
+*/
+
+            for(var i=0; i<response['top'].length; i++) {
+                image_address = RELATIVE_PATH + response['top'][i]['url'];
+                top_address_array.push(image_address);
             }
+
+            for(var i=0; i<response['bottom'].length; i++) {
+                image_address = RELATIVE_PATH + response['bottom'][i]['url'];
+                bottom_address_array.push(image_address);
+            }
+
+            for(var i=0; i<response['onepiece'].length; i++) {
+                image_address = RELATIVE_PATH + response['onepiece'][i]['url'];
+                onepiece_address_array.push(image_address);
+            }
+
+            for(var i=0; i<response['outer'].length; i++) {
+                image_address = RELATIVE_PATH + response['outer'][i]['url'];
+                outer_address_array.push(image_address);
+            } 
 
 
             for(var i=0; i<top_address_array.length; i++) {
                 document.getElementById("mycloset_top"+String(i+1)).src = top_address_array[i];
+                document.getElementById("index_clothes_type_top"+String(i+1)).textContent = response['top'][i]['classname'];
+                document.getElementById("index_clothes_color_top"+String(i+1)).textContent = response['top'][i]['color'];
             }
 
             for(var i=0; i<bottom_address_array.length; i++) {
                 document.getElementById("mycloset_bottom"+String(i+1)).src = bottom_address_array[i];
+                document.getElementById("index_clothes_type_bottom"+String(i+1)).textContent = response['bottom'][i]['classname'];
+                document.getElementById("index_clothes_color_bottom"+String(i+1)).textContent = response['bottom'][i]['color'];
             }
 
             for(var i=0; i<onepiece_address_array.length; i++) {
                 document.getElementById("mycloset_onepiece"+String(i+1)).src = onepiece_address_array[i];
+                document.getElementById("index_clothes_type_onepiece"+String(i+1)).textContent = response['onepiece'][i]['classname'];
+                document.getElementById("index_clothes_color_onepiece"+String(i+1)).textContent = response['onepiece'][i]['color'];
             }
 
             for(var i=0; i<outer_address_array.length; i++) {
                 document.getElementById("mycloset_outer"+String(i+1)).src = outer_address_array[i];
+                document.getElementById("index_clothes_type_outer"+String(i+1)).textContent = response['outer'][i]['classname'];
+                document.getElementById("index_clothes_color_outer"+String(i+1)).textContent = response['outer'][i]['color'];
             }
             
             document.getElementById("closet_ment_username").textContent = response['user_name'] + " 님의 CLOSET";
         }
     })
 }
+
+/*
+function getClosetClass() { // 사용자 의류 클래스 화면에 출력
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:3000/getClosetClass",
+        data: {},
+        success: function (response) {
+            const res = response[0];
+            for(var i=0; i<res.length; i++) {
+                // 종류
+                document.getElementById("index_clothes_type" + String(i+1)).textContent = response[i]['type'];
+
+                // 색상
+                if(response[i]['class'].slice(0, 1) == 'W') {
+                    document.getElementById("index_clothes_color" + String(i+1)).textContent = response[i]['class'].slice(2);    
+                }
+                else {
+                    document.getElementById("index_clothes_color" + String(i+1)).textContent = response[i]['class'].slice(1);
+                }
+            }
+        }
+    })
+}
+*/
